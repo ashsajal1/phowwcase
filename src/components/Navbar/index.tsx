e6a599554f7519e6f0463a7e333889149121d550
ashsajal1 <ashsajal1@gmail.com>
2024-01-09 19:31:53 +0800

    Change nav url

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,11 +5,15 @@ import './index.scss'
 export default function Navbar() {
   return (
     <Flex className='navbar' align={'center'} justify={'between'}>
-        <h3 className='logo'>Phowwcase</h3>
-        <Flex gap={'2'}>
-            <Button><GitHubLogoIcon/> Github repo</Button>
-            <Button variant='soft'>Github repo</Button>
-        </Flex>
+      <h3 className='logo'>Phowwcase</h3>
+      <Flex gap={'2'}>
+        <a href="https://github.com/ashsajal1/phowwcase">
+          <Button><GitHubLogoIcon /> Github repo</Button>
+        </a>
+        <a href="https://x.com/ashsajal1">
+          <Button variant='soft'>Twitter</Button>
+        </a>
+      </Flex>
     </Flex>
   )
 }
