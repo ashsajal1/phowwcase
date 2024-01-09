4bc7102b029bd57b41eb564e0d4b05efc5dc3793
ashsajal1 <ashsajal1@gmail.com>
2024-01-09 14:22:34 +0800

    Create app and add radix ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text, Button } from '@radix-ui/themes';
+export default function App() {
+  return (
+    <div>
+      <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let's go</Button>
+    </Flex>
+    </div>
+  )
+}
